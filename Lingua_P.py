934cf4b761ed94337a6500e3c3bbdae21bb0ecab
Clayton Maciel <62034289+claytonmaciel@users.noreply.github.com>
2020-06-10 15:37:43 -0300

    Update Lingua_P.py

--- Lingua_P.py
+++ Lingua_P.py
@@ -1,3 +1,4 @@
+#forma 1
 frase = input().strip()
 frase = frase.replace("pppp","+") #quando tem 4 pppp é para ser substituído por dois pês
 frase = frase.replace("ppp","-")
@@ -5,3 +6,8 @@ frase = frase.replace("p","")
 frase = frase.replace("-","p")
 frase = frase.replace("+","pp")
 print(frase)
+
+#forma 2
+frase = input()
+palavras = [palavra[1::2] for palavra in frase.split()]
+print(' '.join(palavras))
