9873714eaff435cd1ff75b739398ff765cba56fa
Clayton Maciel <62034289+claytonmaciel@users.noreply.github.com>
2020-08-27 16:11:09 -0300

    Update 2020 - P2F1 - fissura - 2 (melhor solução).py

--- obi 2020/2020 - P2F1 - fissura - 2 (melhor solução).py
+++ obi 2020/2020 - P2F1 - fissura - 2 (melhor solução).py
@@ -11,13 +11,13 @@ while pilha:
     if vulcao[x][y] != '*' and F >= int(vulcao[x][y]):
         vulcao[x][y] = '*'
         if x < N - 1:  # se não atingiu o final da linha, verifica abaixo (larva vai para baixo)
-            pilha.append((x + 1, y))
+            pilha.append((x+1,y))
         if y < N - 1:  # se não atingiu o final da coluna, verifica a direita (larva vai para a direita)
-            pilha.append((x, y+1))
+            pilha.append((x,y+1))
         if x > 0:  # se não atingiu inicio da linha, verifica acima (larva vai para cima)
-            pilha.append((x-1, y))
+            pilha.append((x-1,y))
         if y > 0:  # se não atingiu inicio da coluna, verifica a esquerda (larva vai para esquerda)
-            pilha.append((x, y-1))
+            pilha.append((x,y-1))
 
 for i in range(N):
     print(''.join([str(j) for j in vulcao[i]]))
