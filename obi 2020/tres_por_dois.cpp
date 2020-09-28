83ca977a28b30138e7145caadfdf582f8fe5b9c3
Clayton Maciel <62034289+claytonmaciel@users.noreply.github.com>
2020-09-28 10:26:20 -0300

    Create tres_por_dois.cpp

---
+++ obi 2020/tres_por_dois.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int i,N,sol=0;
+  cin >> N;
+  int ch[N];
+
+  for(i=0;i < N;i++){
+    cin >> ch[i];
+  }
+  
+  sort(ch,ch+N);
+  
+  for(i=N-1;i > -1;i-=3){
+    sol += ch[i]+ch[i-1];
+  }
+
+  cout << sol;
+
+  return 0;
+}
